7384b133d946b1377ace3d9700a3fea41dc9ac35
Adam Wootton <adam@taplytics.com>
2024-03-14 13:38:49 -0400

    update CLI

--- src/action.ts
+++ src/action.ts
@@ -32,7 +32,7 @@ export async function run() {
     const baseBranch = pullRequest.base.ref
     const headBranch = pullRequest.head.ref
 
-    await exec('npm', ['install', '-g', '@devcycle/cli@5.7.0'])
+    await exec('npm', ['install', '-g', '@devcycle/cli@5.14.1'])
 
     const prLink = pullRequest?.html_url
     const prLinkArgs = prLink ? ['--pr-link', prLink] : []
