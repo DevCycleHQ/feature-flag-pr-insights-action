063ffeceb8c659b926f633de0384ea2d4db0b923
Elliot Campbell <elliotgithub@gmail.com>
2024-08-28 16:01:29 -0400

    update cli version to latest

--- src/action.ts
+++ src/action.ts
@@ -32,7 +32,7 @@ export async function run() {
     const baseBranch = pullRequest.base.ref
     const headBranch = pullRequest.head.ref
 
-    await exec('npm', ['install', '-g', '@devcycle/cli@5.14.13'])
+    await exec('npm', ['install', '-g', '@devcycle/cli@5.16.2'])
 
     const prLink = pullRequest?.html_url
     const prLinkArgs = prLink ? ['--pr-link', prLink] : []
